28425c50865db291d3a9fc55d319109b01fee709
meyerz <meyerz@ciandt.com>
2023-11-06 13:52:14 +0800

    nextjs-dashboard practice.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+// const page = () => {
+//   return (
+//     <div>Dashboard Page</div>
+//   )
+// }
+
+// export default page
+
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
